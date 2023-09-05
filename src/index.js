ed789f1572975edfea616fc73971046eb17db462
simonpai <simon.pai@gmail.com>
2023-09-05 12:27:46 +0800

    Fix exports

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 export { default as doggoganger } from './doggoganger.js';
-export * from './browser.js';
+import Api from './api/index.js';
+
+export function buildApi(...args) {
+  return new Api(...args);
+};
