14229cbf4cf88c0f158118cee9a3400f1bf35cc2
simonpai <simon.pai@gmail.com>
2023-09-06 15:20:02 +0800

    Fine tune

--- src/fetch.js
+++ src/fetch.js
@@ -1,32 +1,34 @@
 import { wrapResponse } from './route/utils.js';
 
-export default function buildFetch(api) {
-  return async function fetch(url, { method = 'GET', body } = {}) {
-    method = method.toUpperCase();
-    if (typeof body === 'string') {
-      body = JSON.parse(body);
-    }
-    url = new URL(url);
-    const segments = url.pathname.substring(1).split('/');
-    if (segments[0] === 'v1') {
-      segments.splice(0, 1);
-    }
-    const group = segments[0];
-    let name = segments[1];
-    let type = 'query';
+export default async function fetch(api, url, { method = 'GET', body } = {}) {
+  method = method.toUpperCase();
+  if (typeof body === 'string') {
+    body = JSON.parse(body);
+  }
+  url = new URL(url);
+  const segments = url.pathname.substring(1).split('/');
+  if (segments[0] === 'v1') {
+    segments.splice(0, 1);
+  }
+  const group = segments[0];
+  let name = segments[1];
+  let type = 'query';
 
-    if (group === 'interactions' && segments.length === 1) {
-      name = 'upload';
-      type = 'data';
-    } else if (group === 'ask' && segments.length === 4 && segments[2] === 'questions' && segments[3] === 'answer') {
-      name = 'answer';
-      body = segments[3];
-    }
+  if (group === 'interactions' && segments.length === 1) {
+    name = 'upload';
+    type = 'data';
+  } else if (group === 'ask' && segments.length === 4 && segments[2] === 'questions' && segments[3] === 'answer') {
+    name = 'answer';
+    body = segments[3];
+  }
 
-    if (!api[group][name]) {
-      throw new Error(`Unknown path: ${url.pathname}`);
-    }
+  if (!api[group][name]) {
+    throw new Error(`Unknown path: ${url.pathname}`);
+  }
 
-    return await wrapResponse(api[group][name], type)(body);
-  };
+  const resBody = await wrapResponse(api[group][name], type)(body);
+
+  return new Response(JSON.stringify(resBody), {
+    status: 200,
+  });
 }
